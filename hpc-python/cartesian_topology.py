dc6fa2a0ae8ad508349f5294cd8388baefc5e2bb
Yilu Chen <chenyilu2016@outlook.com>
2021-05-13 22:31:37 +0200

    fix 2.2.2

--- hpc-python/cartesian_topology.py
+++ hpc-python/cartesian_topology.py
@@ -8,4 +8,7 @@ dims = MPI.Compute_dims(size, 2)
 periods = [1,1]
 cartesian2d = comm.Create_cart(dims, periods, reorder= False)
 coord = cartesian2d.Get_coords(rank)
-print(f"In 2d topogy, processor {rank} has coord")
+print(f"In 2d topogy, processor {rank} has coord {coord}")
+left, right = cartesian2d.Shift(direction =0, disp =1)
+top, bottom = cartesian2d.Shift(direction =1, disp =1)
+print(f"In 2d topology, processor {rank} has neighbors: {left}, {right}, {top}, {bottom}. ")
