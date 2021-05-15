1e09ac6863286375fe1a56876908af230ac14181
Yilu Chen <chenyilu2016@outlook.com>
2021-05-15 23:46:02 +0200

    fix 2.4

--- hpc-python/pde-miniapp-py/pde_miniapp_py/data.py
+++ hpc-python/pde-miniapp-py/pde_miniapp_py/data.py
@@ -281,26 +281,22 @@ class Field:
         jend = domain.local_ny-1
         req=[]
         if domain.neighbour_north >= 0: 
-            for i in [0, domain.local_nx]:
-                self._buffN[i] = self(i, jend)
+            self._buffN=self._inner[:,jend].copy()
             req.append(self._comm.Isend(self._buffN, dest = domain.neighbour_north))
             req.append(self._comm.Irecv(self._bdryN, source = domain.neighbour_north))
 
         if domain.neighbour_south >= 0: 
-            for i in [0, domain.local_nx]:
-                self._buffS[i]=self(i,0)
+            self._buffS=self._inner[:,0].copy()
             req.append(self._comm.Isend(self._buffS, dest = domain.neighbour_south))
             req.append(self._comm.Irecv(self._bdryS, source = domain.neighbour_south))
         
         if domain.neighbour_east >= 0:
-            for j in [0, domain.local_ny]:
-                self._buffE[i] = self(iend, j)
+            self._buffE=self._inner[iend,:].copy()
             req.append(self._comm.Isend(self._buffE, dest= domain.neighbour_east))
             req.append(self._comm_Irecv(self._bdryE, source= domain.neighbour_east))
 
         if domain.neighbour_west >= 0:
-            for j in [0, domain.local_ny]:
-                self._buffS[i] = self(0,j)
+            self._buffW=self._inner[0,:].copy()
             req.append(self._comm.Isend(self._buffW, dest=domain.neighbour_west))
             req.append(self._comm_Irecv(self._bdryW, source=domain.neighbour_west))
         
