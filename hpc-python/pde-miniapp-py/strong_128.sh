9e64af4ea4c1784c748038bf407cc57b9800c226
Yilu Chen <chenyilu2016@outlook.com>
2021-05-16 18:20:46 +0200

    run_strong_256.sh

---
+++ hpc-python/pde-miniapp-py/strong_128.sh
@@ -0,0 +1,13 @@
+mpirun -n 1 python3 main.py 128 100 0.005
+
+mpirun -n 2 python3 main.py 128 100 0.005
+
+mpirun -n 4 python3 main.py 128 100 0.005
+
+mpirun -n 8 python3 main.py 128 100 0.005
+
+mpirun -n 12 python3 main.py 128 100 0.005
+
+mpirun -n 16 python3 main.py 128 100 0.005
+
+mpirun -n 32 python3 main.py 128 100 0.005 
