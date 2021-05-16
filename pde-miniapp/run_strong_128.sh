1f432aee757a140fafbbe6a8fc5dda4c208bf748
Yilu Chen <chenyilu2016@outlook.com>
2021-05-16 16:58:58 +0200

    run_strong128.sh

---
+++ pde-miniapp/run_strong_128.sh
@@ -0,0 +1,13 @@
+mpirun -n 1 ./main 128 100 0.005
+
+mpirun -n 2 ./main 128 100 0.005
+
+mpirun -n 4 ./main 128 100 0.005
+
+mpirun -n 8 ./main 128 100 0.005
+
+mpirun -n 12 ./main 128 100 0.005
+
+mpirun -n 16 ./main 128 100 0.005
+
+mpirun -n 32 ./main 128 100 0.005 
