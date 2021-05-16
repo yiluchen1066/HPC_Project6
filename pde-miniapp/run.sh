072189f5406c7cdad764f10521cb0b2a501a95f4
Yilu Chen <chenyilu2016@outlook.com>
2021-05-16 23:47:24 +0200

    change run.sh

--- pde-miniapp/run.sh
+++ pde-miniapp/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-fname="1node_mem1024"
+fname="nnode_mem1024"
 lscpu | grep "Model name" | tee $fname.data
 echo -e "size\tnp\tNewton\tCG\ttime\titers_cg/timespent" | tee -a $fname.data
 
