82fa4bedadb52b9df03ab6f7c342c3c3b2da854a
Yilu Chen <chenyilu2016@outlook.com>
2021-05-16 19:49:32 +0200

    fix run_py.sh

--- hpc-python/pde-miniapp-py/strong_128.sh
+++ hpc-python/pde-miniapp-py/strong_128.sh
@@ -1,13 +1,13 @@
 mpirun -np 1 python3 main.py 128 100 0.005
 
-mpirun -np 1 python3 main.py 128 100 0.005
+mpirun -np 2 python3 main.py 128 100 0.005
 
-mpirun python3 -n 4 main.py 128 100 0.005
+mpirun -np 4 python3 main.py 128 100 0.005
 
-mpirun python3 -n 8 main.py 128 100 0.005
+mpirun -np 8 python3 main.py 128 100 0.005
 
-mpirun python3 -n 12 main.py 128 100 0.005
+mpirun -np 12 python3 main.py 128 100 0.005
 
-mpirun python3 -n 16 main.py 128 100 0.005
+mpirun -np 16 python3 main.py 128 100 0.005
 
-mpirun python3 -n 32 main.py 128 100 0.005
+mpirun -np 32 python3 main.py 128 100 0.005
