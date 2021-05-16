55e5a93ceb35969a244e853fb0fb74437324ac39
Yilu Chen <chenyilu2016@outlook.com>
2021-05-16 19:25:09 +0200

    fix run_py.sh

--- hpc-python/pde-miniapp-py/run_py.sh
+++ hpc-python/pde-miniapp-py/run_py.sh
@@ -2,13 +2,13 @@
 
 fname="1node_mem1024"
 lscpu | grep "Model name" | tee $fname.data
-echo -e "size\tnp\tNewton\tCG\ttime\iters_cg/timespent" | tee -a $fname.data
+echo -e "size\tnp\tNewton\tCG\ttime\titers_cg/timespent" | tee -a $fname.data
 
 n=128
 while [ $n -le 128 ]
 do
     p=1
-    while [ $p -le 32]
+    while [ $p -le 32 ]
     do
         mpirun -np $p python3 main.py $n 100 0.005 | tee -a $fname.data
         ((p+=1))
