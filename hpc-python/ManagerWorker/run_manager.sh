46463d10d691032c36a4008c252edaad50b07b19
Yilu Chen <chenyilu2016@outlook.com>
2021-05-27 00:28:30 +0200

    for loop

--- hpc-python/ManagerWorker/run_manager.sh
+++ hpc-python/ManagerWorker/run_manager.sh
@@ -6,6 +6,8 @@ mpirun -n 8 python3 manager_worker.py 4001 4001 100
 
 mpirun -n 16 python3 manager_worker.py 4001 4001 100 
 
+mpirun -n 32 python3 manager_worker.py 4001 4001 100 
+
 mpirun -n 2 python3 manager_worker.py 4001 4001 50
 
 mpirun -n 4 python3 manager_worker.py 4001 4001 50 
@@ -14,5 +16,5 @@ mpirun -n 8 python3 manager_worker.py 4001 4001 50
 
 mpirun -n 16 python3 manager_worker.py 4001 4001 50 
 
-
+mpirun -n 32 python3 manager_worker.py 4001 4001 100 
 
